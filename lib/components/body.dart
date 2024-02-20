c030d8d249ed7adef6022e3eb0ab493e33a85250
kenan7Th <thbeankenan@gmail.com>
2024-02-20 06:27:23 -0800

    create Home screen

---
+++ lib/components/body.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plantapp/constatnts.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // It will provide us the total height and width of our screen
+
+    Size size = MediaQuery.of(context).size;
+    // it enabel scrolling on small device
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[HeaderWithSearchBox(size: size)],
+      ),
+    );
+  }
+}
+
+class HeaderWithSearchBox extends StatelessWidget {
+  const HeaderWithSearchBox({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: KDefalutPadding * 2.5),
+      //it will cover 20% of our total height
+      height: size.height * 0.2,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+                left: KDefalutPadding,
+                right: KDefalutPadding,
+                bottom: 36 + KDefalutPadding),
+            height: size.height * 0.2 - 27,
+            decoration: BoxDecoration(
+              color: KPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  'Hi Planto!',
+                  style: Theme.of(context).textTheme.headline6!.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                Spacer(),
+                Image.asset("assets/images/logo.png"),
+              ],
+            ),
+          ),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.symmetric(horizontal: KDefalutPadding),
+                padding: EdgeInsets.symmetric(horizontal: KDefalutPadding),
+                height: 54,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: KPrimaryColor.withOpacity(0.23),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          hintText: "Search",
+                          hintStyle: TextStyle(
+                            color: KPrimaryColor.withOpacity(0.5),
+                          ),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          // suffix is not working properly with SVG
+                          //that why we use row
+                        ),
+                      ),
+                    ),
+                    SvgPicture.asset("assets/icons/search-svgrepo-com.svg"),
+                  ],
+                ),
+              ))
+        ],
+      ),
+      color: Colors.black,
+    );
+  }
+}
---
+++ lib/constatnts.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+//colors that we use in our app
+
+const KPrimaryColor = Color(0xFF0C9869);
+const KTextColor = Color(0xFF0C9869);
+
+const KBackgroundColor = Color(0xFF0C9869);
+const double KDefalutPadding = 20.0;
